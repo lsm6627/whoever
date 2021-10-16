85434578d4ea4f3478872545232a2deb576afc1e
Hendrix1995 <hgud55@naver.com>
2021-10-16 22:46:25 +0900

    branch page merge

--- server/index.js
+++ server/index.js
@@ -1,4 +1,24 @@
-"use strict";
+require('dotenv').config();
+const express = require('express');
 
-const Sequelize = require("sequelize");
-const env = process.env.NODE_ENV || "development";
+const cors = require('cors');
+const controllers = require('./controllers');
+const cookieParser = require('cookie-parser');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PUT']
+  })
+);
+app.use(cookieParser());
+// TODO: 아래 형식으로 클라이언트 요청 받는 것 처리 하면 됨
+// app.get('/auth', controllers.auth);
+// app.post('/signup', controllers.signup);
+// app.post('/signin', controllers.signin);
+// app.post('/signout', controllers.signout);
+
+module.exports = con;
