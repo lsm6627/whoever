5d1da55a887c2ebc9c412b96abb928734844248c
Leeseongmin <lsm6627@gmail.com>
2021-10-26 16:44:52 +0900

    시드파일머지준비

--- server/controllers/posts/read_post.js
+++ server/controllers/posts/read_post.js
@@ -1,6 +1,6 @@
-const { json } = require('sequelize/types');
 const { posts } = require('../../models');
 const { users } = require('../../models');
+const { Op } = require('sequelize');
 
 module.exports = {
   // 1번째 : main 에서 10개씩 카테고리별로 가져옴
@@ -22,99 +22,100 @@ module.exports = {
       })
       .catch((err) => res.json(err));
     if (!result) return res.status(404).json('없는 요청입니다');
-    res.status(200).json({
-      title: result.title,
-      createdAt: result.createdAt
-    });
+    console.log('read_post_get;;;', result);
+    // res.status(200).json({
+    //   title: result.title,
+    //   createdAt: result.createdAt
+    // });
     res.status(200).json(result);
-  },
-  // 2번째 : listPage 카테고리 들어가면 나올 목록 10개 나옴
-  get: async (req, res) => {
-    let pageNum = req.query.page;
-    let offset = 0;
-    if (pageNum > 1) {
-      offset = 10 * (pageNum - 1);
-    }
-    const allPostCount = await posts
-      .findAll({
-        where: { categoryId: req.body.categoryId }
-      })
-      .catch((err) => res.json(err));
-    const result = await posts
-      .findAll({
-        where: { categoryId: req.body.categoryId },
-        offset: offset,
-        limit: 10,
-        order: 'createdAt desc',
-        allPostCount: allPostCount.length
-      })
-      .catch((err) => res.json(err));
-    if (!result) return res.status(404).json('없는 요청입니다');
-    res.status(200).json(result);
-  },
-  // 3번째 : onePage 글 안에 들어가면 그 글의 정보 하나
-  // 조회수도 올려주시오
-  get: async (req, res) => {
-    const result = await posts
-      .findOne({
-        where: { id: req.body.id }
-      })
-      .catch((err) => res.json(err));
-    if (!result) return res.status(404).json('없는 요청입니다');
-    res
-      .update({ views: views + 1 })
-      .status(200)
-      .json(result);
-  },
-  // 4번째 : sideBar 작성글 누르면 내가 썼던글 다 나옴ㅋㅋ
-  get: async (req, res) => {
-    const userInfo = await users
-      .findOne({
-        where: { id: req.body.id }
-      })
-      .catch((err) => res.json(err));
-    if (!userInfo) {
-      return res.status(404).json('없는 요청입니다');
-    } else {
-      const result = await posts
-        .findAll({
-          where: { userId: req.body.userId }
-        })
-        .catch((err) => res.json(err));
-      if (!result) {
-        return res.status(404).json('작성한 글이 없습니다');
-      }
-      res.status(200).json(result);
-    }
-  },
-  // 5번째 : 검색하면 검색어가 재목에 들어 있으면 다 나옴!
-  get: async (req, res) => {
-    const keyword = req.body.keyword;
-    const categoryId = req.body.categoryId;
-
-    keyword = keyword.trim();
-    if (!categoryId) {
-      return res.status(404).json('없는 요청 입니다');
-    } else {
-      if (!keyword.length) {
-        return res.status(404).json('검색어를 입력해 주세요');
-      } else {
-        keyword = keyword.replace(/\s\s+/gi, ' ');
-        const result = await posts
-          .findAll({
-            attributes: ['title', 'categoryId'],
-            where: {
-              categoryId: categoryId,
-              title: { [like]: `%${keyword}%` }
-            }
-          })
-          .catch((err) => json(err));
-        if (!result) {
-          return res.status(404).json('작성한 글이 없습니다');
-        }
-        res.status(200).json(result);
-      }
-    }
   }
+  // 2번째 : listPage 카테고리 들어가면 나올 목록 10개 나옴
+  // get: async (req, res) => {
+  //   let pageNum = req.query.page;
+  //   let offset = 0;
+  //   if (pageNum > 1) {
+  //     offset = 10 * (pageNum - 1);
+  //   }
+  //   const allPostCount = await posts
+  //     .findAll({
+  //       where: { categoryId: req.body.categoryId }
+  //     })
+  //     .catch((err) => res.json(err));
+  //   const result = await posts
+  //     .findAll({
+  //       where: { categoryId: req.body.categoryId },
+  //       offset: offset,
+  //       limit: 10,
+  //       order: 'createdAt desc',
+  //       allPostCount: allPostCount.length
+  //     })
+  //     .catch((err) => res.json(err));
+  //   if (!result) return res.status(404).json('없는 요청입니다');
+  //   res.status(200).json(result);
+  // },
+  // // 3번째 : onePage 글 안에 들어가면 그 글의 정보 하나
+  // // 조회수도 올려주시오
+  // get: async (req, res) => {
+  //   const result = await posts
+  //     .findOne({
+  //       where: { id: req.body.id }
+  //     })
+  //     .catch((err) => res.json(err));
+  //   if (!result) return res.status(404).json('없는 요청입니다');
+  //   res
+  //     .update({ views: views + 1 })
+  //     .status(200)
+  //     .json(result);
+  // },
+  // // 4번째 : sideBar 작성글 누르면 내가 썼던글 다 나옴ㅋㅋ
+  // get: async (req, res) => {
+  //   const userInfo = await users
+  //     .findOne({
+  //       where: { id: req.body.id }
+  //     })
+  //     .catch((err) => res.json(err));
+  //   if (!userInfo) {
+  //     return res.status(404).json('없는 요청입니다');
+  //   } else {
+  //     const result = await posts
+  //       .findAll({
+  //         where: { userId: req.body.userId }
+  //       })
+  //       .catch((err) => res.json(err));
+  //     if (!result) {
+  //       return res.status(404).json('작성한 글이 없습니다');
+  //     }
+  //     res.status(200).json(result);
+  //   }
+  // },
+  // // 5번째 : 검색하면 검색어가 재목에 들어 있으면 다 나옴!
+  // get: async (req, res) => {
+  //   const keyword = req.body.keyword;
+  //   const categoryId = req.body.categoryId;
+
+  //   keyword = keyword.trim();
+  //   if (!categoryId) {
+  //     return res.status(404).json('없는 요청 입니다');
+  //   } else {
+  //     if (!keyword.length) {
+  //       return res.status(404).json('검색어를 입력해 주세요');
+  //     } else {
+  //       keyword = keyword.replace(/\s\s+/gi, ' ');
+  //       const result = await posts
+  //         .findAll({
+  //           attributes: ['title', 'categoryId'],
+  //           where: {
+  //             categoryId: categoryId,
+  //             title: { [like]: `%${keyword}%` }
+  //           }
+  //         })
+  //         .catch((err) => json(err));
+  //       if (!result) {
+  //         return res.status(404).json('작성한 글이 없습니다');
+  //       }
+  //       res.status(200).json(result);
+  //     }
+  //   }
+  // }
   // TODO: 6번째 : 제목 + 내용 검색 기능 or 제목만 찾기 기능 분리
 };
--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const controllers = require('./controllers');
 const cookieParser = require('cookie-parser'); // TODO: 언젠가 이걸 지울지 몰라.
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(
   session({
@@ -37,27 +37,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // users 요청
-app.post('/login', controllers.login);
-app.post('/logout', controllers.logout);
-app.post('/signup', controllers.signup);
-app.get('/userinfo', controllers.userinfo);
+app.post('/login', controllers.login.post);
+app.post('/logout', controllers.logout.post);
+app.post('/signup', controllers.signup.post);
+app.get('/userinfo', controllers.userinfo.get);
 
 // posts 요청
-app.get('/main', controllers.readpost);
-app.get('/listpage', controllers.readpost);
-app.get('/onepost', controllers.readpost);
-app.get('/mypost', controllers.readpost);
-app.get('/search', controllers.readpost);
-app.update('/changepost', controllers.changepost);
-app.delete('/deletepost', controllers.deletepost);
-app.post('/uploadpost', controllers.uploadpost);
-app.post('/suggestionsup', controllers.suggestions);
-app.post('/suggestionsdown', controllers.suggestions);
+app.get('/main', controllers.readpost.get);
+// app.get('/listpage', controllers.readpost);
+// app.get('/onepost', controllers.readpost);
+// app.get('/mypost', controllers.readpost);
+// app.get('/search', controllers.readpost);
+// app.update('/changepost', controllers.changepost);
+// app.delete('/deletepost', controllers.deletepost);
+// app.post('/uploadpost', controllers.uploadpost);
+// app.post('/suggestionsup', controllers.suggestions);
+// app.post('/suggestionsdown', controllers.suggestions);
 
 // comments 요청
-app.update('/changecomment', controllers.changecomment);
-app.delete('/deletecomment', controllers.deletecomment);
-app.get('/getcomments', controllers.getcomments);
-app.post('/uploadcomment', controllers.uploadcomment);
+// app.update('/changecomment', controllers.changecomment);
+// app.delete('/deletecomment', controllers.deletecomment);
+// app.get('/getcomments', controllers.getcomments);
+// app.post('/uploadcomment', controllers.uploadcomment);
 
 app.listen(PORT, () => console.log(`this server listening on ${PORT}`));
