634b9cd0a938f425f36de9036293a05ae146b953
Hendrix1995 <hgud55@naver.com>
2021-10-28 01:39:17 +0900

    fix: login can't get response

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,127 +1,130 @@
-import React, {useEffect,useState} from "react";
+import React, { useEffect, useState } from 'react';
 // // import axios from "axios";
 
- import {useForm} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
- import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
- import {
-   SignUpModalContainer,
-   SignUpModalBtn,
-   SignUpModalBackdrop,
-   SignUpModalView,
-   CloseBtn,
-   SignUpContentContainer,
-   TitleDiv,
-   Idtext, 
-   Id_Input,
-   Id_Validation_Check,
-   Pwtext, 
-   Pw_Input,
-   PwValidLenMatch,
-   PwConfirmtext,   
-   Pw_ReInput,
-   PwNotMatch,
-   JoinBtn,
-   } from './SignUp.style';
+import {
+  SignUpModalContainer,
+  SignUpModalBtn,
+  SignUpModalBackdrop,
+  SignUpModalView,
+  CloseBtn,
+  SignUpContentContainer,
+  TitleDiv,
+  Idtext,
+  Id_Input,
+  Id_Validation_Check,
+  Pwtext,
+  Pw_Input,
+  PwValidLenMatch,
+  PwConfirmtext,
+  Pw_ReInput,
+  PwNotMatch,
+  JoinBtn
+} from './SignUp.style';
 
-   const SignUp = () => {
+const SignUp = () => {
+  const history = useHistory();
 
-    const history = useHistory();
+  const [isOpen, setIsOpen] = useState(false);
+  const [loginLen, setLoginLen] = useState(false);
+  const [isVerified, setIsVerified] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false);
-    const [loginLen, setLoginLen] = useState(false);
-    const [isVerified, setIsVerified] = useState(false);
-    
-     const {register,handleSubmit,formState: {errors}, watch} = useForm();
-    const onSubmit = (data) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
 
-    useEffect(() =>{
-      setIsVerified(watch('verifyPassword') === watch('password'));
-    }, [watch('verifyPassword'), watch('password')]);
-    
-    const openModalHandler = () => {
-       setIsOpen(!isOpen);
-     };
+  useEffect(() => {
+    setIsVerified(watch('verifyPassword') === watch('password'));
+  }, [watch('verifyPassword'), watch('password')]);
 
-    return (
-     <SignUpModalContainer onSubmit={handleSubmit(onSubmit)}>
-        
-        <SignUpModalBtn onClick={openModalHandler}>
+  const openModalHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <SignUpModalContainer onSubmit={handleSubmit(onSubmit)}>
+      <SignUpModalBtn onClick={openModalHandler}>
         {isOpen === false ? '아직 계정이 없다면?' : '아직 계정이 없다면?'}
-     </SignUpModalBtn>
-        
-      {isOpen === true ? (
-         <SignUpModalBackdrop onClick={openModalHandler}>
-           <SignUpModalView onClick={(e) => e.stopPropagation()}>
-           <CloseBtn 
-           className="fas fa-times-circle" 
-           onClick={openModalHandler}>
-              </CloseBtn>
-              <SignUpContentContainer onSubmit={handleSubmit(onSubmit)}>
-             <TitleDiv>WHOEVER Sign Up</TitleDiv>
-           <Idtext>Whoever ID</Idtext>
-           <Id_Input 
-           name="userId"
-           className="userId"
-           {...register('userId', {
-             pattern: /^[a-z0-9_-]{4,20}$/,
-             maxLength: 20,
-             minLength: 4,
-             required: true
-           })}/>
-          <Pwtext>Password</Pwtext>  
-           <Pw_Input 
-           name="password"
-           className="password"
-           {...register('password', {
-           pattern: /(?=.*\d)(?=.*[a-zA-ZS]).{8,}/,
-           required: true,
-           minLength: 8
-           })}
-           onInvalid={(e) => {
-             e.target.setCustomValidity(
-               '비밀번호는 8글자 이상, 영문, 숫자 조합이어야 합니다.'
-             );
-           }}
-           onInput={(e) => {
-             e.target.setCustomValidity('');
-           }}
-         />
-         <PwConfirmtext>Password Confirm</PwConfirmtext>
-           <Pw_ReInput 
-           name="verifyPassword"
-           className="password"
-           {...register('verifyPassword', { required: true })}
-           onInvalid={(e) => {
-             e.target.setCustomValidity('비밀번호가 일치하지 않습니다.');
-           }}
-           onInput={(e) => {
-             e.target.setCustomValidity('');
-           }}
-           />
-            {errors.userId 
-            && errors.userId.type === "required" 
-            && <Id_Validation_Check>
-               아이디는 소문자, 숫자 4~20 글자여야 합니다.
-             </Id_Validation_Check>}
-              {errors.password
-              && errors.password.type === "required"
-              &&
-             <PwValidLenMatch>
-               비밀번호는 8글자 이상, 영문, 숫자 조합이어야 합니다.
-             </PwValidLenMatch>
-              }
-              {errors.verifyPassword 
-              &&errors.verifyPassword.type === "required"
-              &&<PwNotMatch>비밀번호가 일치하지 않습니다.</PwNotMatch>}
-              <JoinBtn>Sign Up</JoinBtn>
-           </SignUpContentContainer>
-         </SignUpModalView>
-       </SignUpModalBackdrop>
-       ) : null}
-     </SignUpModalContainer>
-  )
-}
+      </SignUpModalBtn>
 
- export default SignUp
+      {isOpen === true ? (
+        <SignUpModalBackdrop onClick={openModalHandler}>
+          <SignUpModalView onClick={(e) => e.stopPropagation()}>
+            <CloseBtn
+              className="fas fa-times-circle"
+              onClick={openModalHandler}
+            ></CloseBtn>
+            <SignUpContentContainer onSubmit={handleSubmit(onSubmit)}>
+              <TitleDiv>WHOEVER Sign Up</TitleDiv>
+              <Idtext>Whoever ID</Idtext>
+              <Id_Input
+                name="userId"
+                className="userId"
+                {...register('userId', {
+                  pattern: /^[a-z0-9_-]{4,20}$/,
+                  maxLength: 20,
+                  minLength: 4,
+                  required: true
+                })}
+              />
+              <Pwtext>Password</Pwtext>
+              <Pw_Input
+                name="password"
+                className="password"
+                {...register('password', {
+                  pattern: /(?=.*\d)(?=.*[a-zA-ZS]).{8,}/,
+                  required: true,
+                  minLength: 8
+                })}
+                onInvalid={(e) => {
+                  e.target.setCustomValidity(
+                    '비밀번호는 8글자 이상, 영문, 숫자 조합이어야 합니다.'
+                  );
+                }}
+                onInput={(e) => {
+                  e.target.setCustomValidity('');
+                }}
+              />
+              <PwConfirmtext>Password Confirm</PwConfirmtext>
+              <Pw_ReInput
+                name="verifyPassword"
+                className="password"
+                {...register('verifyPassword', { required: true })}
+                onInvalid={(e) => {
+                  e.target.setCustomValidity('비밀번호가 일치하지 않습니다.');
+                }}
+                onInput={(e) => {
+                  e.target.setCustomValidity('');
+                }}
+              />
+              {errors.userId && errors.userId.type === 'required' && (
+                <Id_Validation_Check>
+                  아이디는 소문자, 숫자 4~20 글자여야 합니다.
+                </Id_Validation_Check>
+              )}
+              {errors.password && errors.password.type === 'required' && (
+                <PwValidLenMatch>
+                  비밀번호는 8글자 이상, 영문, 숫자 조합이어야 합니다.
+                </PwValidLenMatch>
+              )}
+              {errors.verifyPassword &&
+                errors.verifyPassword.type === 'required' && (
+                  <PwNotMatch>비밀번호가 일치하지 않습니다.</PwNotMatch>
+                )}
+              <JoinBtn>Sign Up</JoinBtn>
+            </SignUpContentContainer>
+          </SignUpModalView>
+        </SignUpModalBackdrop>
+      ) : null}
+    </SignUpModalContainer>
+  );
+};
+
+export default SignUp;
