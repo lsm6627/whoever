4c0f312f31770a15e6daa4deb436a4fad2a1f0d2
Leeseongmin <lsm6627@gmail.com>
2021-11-01 00:51:07 +0900

    pull받기위해 다삭제

--- server/config/config.js
+++ server/config/config.js
@@ -2,12 +2,18 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const config = {
+  development: {
+    host: 'localhost',
+    username: 'root',
+    password: process.env.DATABASE_PASSWORD,
+    database: 'whoever_test',
+    dialect: 'mysql'
+  },
   production: {
     host: 'localhost',
     username: 'admin',
     password: process.env.DATABASE_PASSWORD,
     database: 'whoever',
-    port: 80,
     dialect: 'mysql'
   }
 };
